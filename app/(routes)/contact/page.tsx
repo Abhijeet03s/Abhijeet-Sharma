767ce1f2b1a5705d8888d92239aa7187b4e89dd0
Abhijeet03s <abhijeetsharma476@gmail.com>
2025-01-26 23:57:32 +0530

    Update contact us page

--- app/(routes)/contact/page.tsx
+++ app/(routes)/contact/page.tsx
@@ -7,8 +7,7 @@ export default function ContactPage() {
          <div className="space-y-4">
             <h1 className="text-xl font-bold">Let&apos;s Create Something Awesome Together! 🚀</h1>
             <p className="text-gray-300 text-base leading-relaxed">
-               Got an idea or a project in mind?
-               Drop me a DM on Twitter—I&apos;d love to chat and bring it to life!
+               Got something cool to share? Slide into my <a href="https://x.com/iabhi43" className="text-blue-500">Twitter</a> or drop an <a href="mailto:abhiijeetsharma476@gmail.com" className="text-blue-500">email</a>—it’s where the magic happens! ✨📩
             </p>
          </div>
       </div>
