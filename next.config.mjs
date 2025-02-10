466282f472ef7d906ebc20944b865b9adfb66432
Abhijeet03s <abhijeetsharma476@gmail.com>
2025-02-10 17:51:30 +0530

    Add redirect for /resume to Google Drive

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+   async redirects() {
+      return [
+         {
+            source: '/resume',
+            destination: 'https://drive.google.com/file/d/19Q3NLTqAVZJvp3nlcx8n26DmTkOEQj2d/view',
+            permanent: true,
+         },
+      ];
+   },
+};
 
-export default nextConfig;
+module.exports = nextConfig;
